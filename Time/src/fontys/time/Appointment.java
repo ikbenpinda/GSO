44893381c9ac8022a938158c83bc59c58335d39f
Roel de Wit <rthdewit@gmail.com>
2014-10-11 14:32:30 +0200

    Finished Contact and Appointment classes

--- Time/src/fontys/time/Appointment.java
+++ Time/src/fontys/time/Appointment.java
@@ -5,10 +5,48 @@
  */
 package fontys.time;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author Roel
  */
 public class Appointment {
-    
+
+    String subject;
+    IPeriod period;
+    ArrayList<Contact> invitees;
+
+    public Appointment(String subject, IPeriod period) {
+        this.subject = subject;
+        this.period = period;
+        invitees = new ArrayList<Contact>();
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public IPeriod getPeriod() {
+        return period;
+    }
+
+    public Iterator<Contact> invitees() {
+        return invitees.iterator();
+    }
+
+    public boolean addContact(Contact contact) {
+        boolean result = contact.addAppointment(this);
+
+        if (result) {
+            invitees.add(contact);
+        }
+        
+        return result;
+    }
+
+    public void removeContact(Contact contact) {
+        invitees.remove(contact);
+    }
 }
--- Time/src/fontys/time/Contact.java
+++ Time/src/fontys/time/Contact.java
@@ -5,10 +5,46 @@
  */
 package fontys.time;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author Roel
  */
 public class Contact {
+
+    String name;
+    ArrayList<Appointment> agenda;
+
+    public Contact(String name) {
+        this.name = name;
+        agenda = new ArrayList<Appointment>();
+    }
+
+    public String getName() {
+        return name;
+    }
     
+    boolean addAppointment(Appointment appointment) {
+        if (appointment != null) {
+            for (Appointment app : agenda) {
+                if (app.getPeriod().intersectionWith(appointment.getPeriod()) != null) {
+                    return false;
+                }
+            }
+            agenda.add(appointment);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void removeAppointment(Appointment appointment) {
+        agenda.remove(appointment);
+    }
+
+    public Iterator<Appointment> appointments() {
+        return agenda.iterator();
+    }
 }
